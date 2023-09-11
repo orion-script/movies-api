758afda2a65effc4cd4b36f2e7b629730c7e1607
Hadestech01 <jaolaniyi96@student.lautech.edu.ng>
2023-09-11 17:55:14 +0100

    another commit

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -3,15 +3,15 @@ import { AiFillFacebook } from "react-icons/ai";
 import { BsInstagram, BsTwitter, BsYoutube } from "react-icons/bs";
 function footer() {
   return (
-    <div className="w-2/4 m-auto h-auto mt-10 flex flex-col text-center font-sans py-5">
+    <div className="w-11/12 md:w-2/4 m-auto h-auto mt-10 flex flex-col text-center font-sans py-5 font-mono">
       <div className="flex items-center w-4/5 m-auto justify-between">
-        <AiFillFacebook />
-        <BsInstagram />
-        <BsTwitter />
-        <BsYoutube />
+        <AiFillFacebook className="w-8 h-8 shadow-lg" />
+        <BsInstagram className="w-8 h-8 shadow-lg" />
+        <BsTwitter className="w-8 h-8 shadow-lg" />
+        <BsYoutube className="w-8 h-8 shadow-lg" />
       </div>
 
-      <ul className="flex items-center my-5 w-4/5 m-auto justify-between">
+      <ul className="flex items-center my-5 w-4/5 m-auto justify-between text-sm md:text-lg">
         <li>
           <a href="#">Conditions of Use</a>
         </li>
@@ -23,9 +23,16 @@ function footer() {
         </li>
       </ul>
 
-      <p>
+      <p className="text-medium">
         2023 MovieBox by{" "}
-        <a href="https://hades-portfolio.vercel.app/">HadesCodes</a>
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://hades-portfolio.vercel.app/"
+          className="text-red-700 text-base font-thin border-b-2 border-gray-800"
+        >
+          HadesCodes
+        </a>
       </p>
     </div>
   );
