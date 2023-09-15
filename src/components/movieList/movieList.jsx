79fdeaf247239078a7c7eba4113c85261df7b183
Hadestech01 <jaolaniyi96@student.lautech.edu.ng>
2023-09-15 17:29:22 +0100

    Life hard ooo

--- src/components/movieList/movieList.jsx
+++ src/components/movieList/movieList.jsx
@@ -18,7 +18,7 @@ const MovieList = () => {
   const getData = () => {
     fetch(
       `https://api.themoviedb.org/3/movie/${
-        type ? type : "popular"
+        type ? type : "top_rated"
       }?api_key=4e44d9029b1270a757cddc766a1bcb63&language=en-US`
     )
       .then((res) => res.json())
